6384695b783f67741e6fe2af2284340df6347d4c
manohari268 <manoharimb5@gmail.com>
2026-06-11 15:07:46 +0530

    Add Gradle Java application

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello CodeAlpha!");
+        System.out.println("Java Application using Gradle");
+    }
+}
